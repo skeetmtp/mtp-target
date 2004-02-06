c3b0825792aff044d611ffcffcc220edd2a025ba
Alban Lecocq <skeetmtp@gmail.com>
2004-02-06 21:56:20 +0000

    *** empty log message ***

--- mtp-target/server/data/level/level_team.lua
+++ mtp-target/server/data/level/level_team.lua
@@ -25,31 +25,37 @@ Cameras =
 
 StartPoints =
 {
-	CVector(-0.098865, 1.56397, 9.68978),
-	CVector(-0.25109, 1.56534, 9.68978),
-	CVector(-0.411029, 1.55867, 9.68978),
-	CVector(-0.570633, 1.55203, 9.68978),
-	CVector(0.0733468, 1.5695, 9.68978),
-	CVector(0.225997, 1.56282, 9.68978),
-	CVector(0.394393, 1.55616, 9.68978),
-	CVector(0.554403, 1.54953, 9.68978),
-	CVector(-0.178982, 1.3806, 9.68978),
-	CVector(-0.331207, 1.38197, 9.68978),
-	CVector(-0.491146, 1.37531, 9.68978),
-	CVector(-0.65075, 1.36866, 9.68978),
-	CVector(-0.00677051, 1.38614, 9.68978),
-	CVector(0.14588, 1.37946, 9.68978),
-	CVector(0.314275, 1.3728, 9.68978),
-	CVector(0.474286, 1.36616, 9.68978)
+	CVector(-0.098865, 16.56397, 9.68978),
+	CVector(-0.25109,  16.56534, 9.68978),
+	CVector(-0.411029, 16.55867, 9.68978),
+	CVector(-0.570633, 16.55203, 9.68978),
+	CVector(0.0733468, 16.5695, 9.68978),
+	CVector(0.225997,  16.56282, 9.68978),
+	CVector(0.394393,  16.55616, 9.68978),
+	CVector(0.554403,  16.54953, 9.68978),
+	CVector(-0.178982, 16.3806, 9.68978),
+	CVector(-0.331207, 16.38197, 9.68978),
+	CVector(-0.491146, 16.37531, 9.68978),
+	CVector(-0.65075,  16.36866, 9.68978),
+	CVector(-0.00677051, 16.38614, 9.68978),
+	CVector(0.14588,  16.37946, 9.68978),
+	CVector(0.314275, 16.3728, 9.68978),
+	CVector(0.474286, 16.36616, 9.68978)
 }
 
 Modules =
 {
-	{ Position = CVector(10, -25, 3.5),Rotation = CAngleAxis(1,0,0,0),			Name = "snow_island" },
-	{ Position = CVector(6, -14, 2.8),Rotation = CAngleAxis(1,0,0,0),			Name = "snow_island2" },
-	{ Position = CVector(-3.8, -17.0, 2),Rotation = CAngleAxis(1,0,0,0),		Name = "snow_island3" },
-	{ Position = CVector(-0.1, -15.3, 1),Rotation = CAngleAxis(1,0,0,0),		Name = "snow_target_50" },
-	{ Position = CVector(-0.1, -15.35, 2.55),Rotation = CAngleAxis(1,0,0,0),	Name = "snow_target_100" },
-	{ Position = CVector(-0.1, -15.33, 2.8),Rotation = CAngleAxis(1,0,0,0),	Name = "snow_target_300" },
-	{ Position = CVector(0, 0, 5),Rotation = CAngleAxis(1,0,0,0),				Name = "snow_ramp" }
+	{ Position = CVector(10.000000,-10.000000,3.500000),Rotation = CAngleAxis(1.000000,0.000000,0.000000,0.000000), Name="snow_island" },
+	{ Position = CVector(6.000000,1.000000,2.800000),Rotation = CAngleAxis(1.000000,0.000000,0.000000,0.000000), Name="snow_island2" },
+	{ Position = CVector(-3.800000,-2.000000,2.000000),Rotation = CAngleAxis(1.000000,0.000000,0.000000,0.000000), Name="snow_island3" },
+	{ Position = CVector(0.397904,-0.040334,3.0),Rotation = CAngleAxis(1.000000,0.000000,0.000000,0.000000), Name="team_target_50_red" },
+	{ Position = CVector(0.400000,-0.257311,3.0),Rotation = CAngleAxis(1.000000,0.000000,0.000000,0.000000), Name="team_target_100_red" },
+	{ Position = CVector(0.400843,-0.369995,3.0),Rotation = CAngleAxis(1.000000,0.000000,0.000000,0.000000), Name="team_target_300_red" },
+	{ Position = CVector(0.400000,-0.782447,3.0),Rotation = CAngleAxis(1.000000,0.000000,0.000000,0.000000), Name="team_target_50_blue" },
+	{ Position = CVector(0.400000,-0.558576,3.0),Rotation = CAngleAxis(1.000000,0.000000,0.000000,0.000000), Name="team_target_100_blue" },
+	{ Position = CVector(0.400000,-0.445013,3.0),Rotation = CAngleAxis(1.000000,0.000000,0.000000,0.000000), Name="team_target_300_blue" },
+	{ Position = CVector(0.000000,15.000000,5.000000),Rotation = CAngleAxis(1.000000,0.000000,0.000000,0.000000), Name="snow_ramp" },
+	{ Position = CVector(0.000000,-15.000000,5.000000),Rotation = CAngleAxis(0.000000,0.000000,1.000000,3.141500), Name="snow_ramp" }
 }
+
+
