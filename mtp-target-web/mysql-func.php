c3bedc93e001ca7dd521779006e30265066974e2
Alban Lecocq <skeetmtp@gmail.com>
2004-02-11 14:16:36 +0000

    FIXED : missing $base

--- mtp-target-web/mysql-func.php
+++ mtp-target-web/mysql-func.php
@@ -5,7 +5,7 @@ function exec_requete($requete)
 {
 	$MySqlLinkIdentifier = mysql_connect("$host","$base","$password") or die($die_message);
 
-	mysql_select_db("mtptarget") or die($die_message);
+	mysql_select_db("$base") or die($die_message);
 
 	$resultat = mysql_query($requete) or die($die_message);
 	$erreur = mysql_error();
