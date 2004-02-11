0632f5599b9e9184ef458b4c095b3158fb0af7bd
Alban Lecocq <skeetmtp@gmail.com>
2004-02-11 17:34:24 +0000

    *** empty log message ***

--- mtp-target-web/config.default.php
+++ mtp-target-web/config.default.php
@@ -1,5 +1,17 @@
 <?php
-   $host="localhost";    
-   $base="mybase";    
-   $password="mypassword"; 
+	function exec_requete($requete)
+	{
+	
+		$MySqlLinkIdentifier = mysql_connect( "localhost", "base", "password" ) or die($die_message);
+	
+		mysql_select_db("base") or die($die_message);
+	
+		$resultat = mysql_query($requete) or die($die_message);
+		$erreur = mysql_error();
+		if ($erreur != "")
+		{
+	    		die($die_message);
+		}
+		return $resultat;
+	}
 ?>
