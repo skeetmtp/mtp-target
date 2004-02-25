eebe6f58b050e3d0a90b7e11c80c28096b69f453
Alban Lecocq <skeetmtp@gmail.com>
2004-02-25 14:46:11 +0000

    *** empty log message ***

--- mtp-target-web/download.php
+++ mtp-target-web/download.php
@@ -7,7 +7,7 @@ echo "<h3>$menuLinkText_Download</h3>";
 ?>
 
 <table border="0">
-	<tr><td bgcolor="<?php echo $table_head_color;?>"><b>&nbsp;Full version&nbsp;</b></td></tr>
+	<tr><td bgcolor="<?php echo $table_news_head_color;?>"><b>&nbsp;Full version&nbsp;</b></td></tr>
 	<?php
 
 		$dirname = "./download/";
@@ -15,7 +15,6 @@ echo "<h3>$menuLinkText_Download</h3>";
 		$count = 0;
 		while (false !== ($entry = $d->read())) 
 		{
-			$count++;
 			if(($count%2)==0)
 				$color = $table_row_even_color;
 			else
@@ -28,14 +27,22 @@ echo "<h3>$menuLinkText_Download</h3>";
 				if(strstr($entry,"update")=="")
 				{
 					printf("<tr>");
-					printf("<td bgcolor=\"%s\">&nbsp;<a href=\"$f\"><img src=\"download.png\" ALT=\"file\"> $entry</a>&nbsp;</td>",$color);
+					printf("<td bgcolor=\"%s\">&nbsp;<a href=\"$f\"><img src=\"download.png\" ALT=\"file\"> $entry</a>&nbsp;</td>",$table_news_row_color);
+					printf("</tr>");
+					$count++;
 				}
 			}
 		}
 		$d->close();
+		if($count==0)
+		{
+			printf("<tr>");
+			printf("<td bgcolor=\"%s\">No file available</td>",$table_news_row_color);
+			printf("</tr>");
+		}
 	?>
 	<tr><td><b>&nbsp;&nbsp;</b></td></tr>
-	<tr><td bgcolor="<?php echo $table_head_color;?>"><b>&nbsp;update only&nbsp;</b></td></tr>
+	<tr><td bgcolor="<?php echo $table_news_head_color;?>"><b>&nbsp;Update&nbsp;</b></td></tr>
 	<?php
 
 		$dirname = "./download/";
@@ -43,7 +50,6 @@ echo "<h3>$menuLinkText_Download</h3>";
 		$count = 0;
 		while (false !== ($entry = $d->read())) 
 		{
-			$count++;
 			if(($count%2)==0)
 				$color = $table_row_even_color;
 			else
@@ -56,11 +62,19 @@ echo "<h3>$menuLinkText_Download</h3>";
 				if(strstr($entry,"update")!="")
 				{
 					printf("<tr>");
-					printf("<td bgcolor=\"%s\">&nbsp;<a href=\"$f\"><img src=\"download.png\" ALT=\"file\"> $entry</a>&nbsp;</td>",$color);
+					printf("<td bgcolor=\"%s\">&nbsp;<a href=\"$f\"><img src=\"download.png\" ALT=\"file\"> $entry</a>&nbsp;</td>",$table_news_row_color);
+					printf("</tr>");
+					$count++;
 				}
 			}
 		}
 		$d->close();
+		if($count==0)
+		{
+			printf("<tr>");
+			printf("<td bgcolor=\"%s\">No file available</td>",$table_news_row_color);
+			printf("</tr>");
+		}
 	?>
 	<tr><td><b>&nbsp;&nbsp;</b></td></tr>
 </table>
