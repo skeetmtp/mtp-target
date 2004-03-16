59d2d196eaa38bf222538e5d9ec409125b865d15
Alban Lecocq <skeetmtp@gmail.com>
2004-03-16 21:06:12 +0000

    CHANGED : web stats url

--- mtp-target-web/stats.php
+++ mtp-target-web/stats.php
@@ -2,8 +2,8 @@
 ?>
 <h3>Stats</h3>
 <ul>
-<li><a href="http://stats.tuxfamily.net/mtp-target.tuxfamily.org/">Web site stats</a></li>
-<li><a href="http://195.68.21.201/cgi-bin/awstats.pl">login stats</a></li>
+<li><a href="http://stats.mtp-target.org/">Web site stats</a></li>
+<li><a href="http://mtp-target.dyndns.org/cgi-bin/awstats.pl">login stats</a></li>
 <li><a href="http://cia.navi.cx/stats/project/mtp-target">CVS Stats</a></li>
 </ul>
 
