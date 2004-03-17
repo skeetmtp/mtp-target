385689a51579def9cbe23fe25e58cde39df62f77
Alban Lecocq <skeetmtp@gmail.com>
2004-03-17 16:51:30 +0000

    *** empty log message ***

--- mtp-target-web/source.php
+++ mtp-target-web/source.php
@@ -1,8 +1,10 @@
 <?php
 
   printf("<a href=\"http://cvs.gna.org/viewcvs/mtptarget/\">%s</a><br>","browse cvs");
-  echo "get cvs :"
-  echo "cvs -d:pserver:anonymous@cvs.gna.org:/cvs/mtptarget co mtp-target"
+  echo "<br>";
+  echo "get cvs :<br>";
+  echo "cvs -d:pserver:anonymous@cvs.gna.org:/cvs/mtptarget co mtp-target<br>";
+  echo "<br>";
   printf("<a href=\"http://cvs.gna.org/daily/mtptarget.tar.gz\">%s</a><br>","download daily tarball");
 
 ?>
