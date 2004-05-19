e6b5136fb55bb346efacaa248eafebbfca7fcb16
Vianney Lecroart <acemtp@gmail.com>
2004-05-19 18:54:49 +0000

    CHANGED: formatage

--- mtp-target/www/view_custom.php
+++ mtp-target/www/view_custom.php
@@ -17,36 +17,27 @@
 
 		$dirname = "../user_texture/";
 		$d = dir($dirname);
-		$count = 0;
 		while (false !== ($entry = $d->read())) 
 		{
-			if(($count%2)==0)
-				$color = $table_row_even_color;
-			else
-				$color = $table_row_odd_color;
-			
 			$f = $dirname . $entry ;
 			$t = filetype($f);
 			if($t == "file")
 			{
 				if(strstr($entry,"ping_ball_")!="" && strstr($entry,".tga")!="" )
 				{
+					$modified = stat("../user_texture/$entry");
+					$moddate = date("D M j G:i:s T Y",$modified[9]);
 					printf("<tr>");
 					$entryjpg = str_replace(".tga", ".jpg", $entry);
-					printf("<td bgcolor=\"%s\"><a href=\"../user_texture/$entry\"><img src=\"../user_texture/$entryjpg\" ALT=\"file\"></a>&nbsp;$entry&nbsp;</td>",$table_news_row_color);
-					printf("</tr>");
+					printf("<td><a href=\"../user_texture/$entry\"><img src=\"../user_texture/$entryjpg\" ALT=\"file\"></a></td>");
+					echo "<td><table><tr><td>Name: $entry</td></tr>";
+					echo "<tr><td>Uploaded: $moddate</td></tr></table>";
+					printf("</td></tr>");
 					printf("\n");
-					$count++;
 				}
 			}
 		}
 		$d->close();
-		if($count==0)
-		{
-			printf("<tr>");
-			printf("<td bgcolor=\"%s\">No file </td>",$table_news_row_color);
-			printf("</tr>");
-		}
 	?>
 </table>
 
