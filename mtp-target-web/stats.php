cb5b0ac00f1d11a9b50d1cb4c000f74a30689797
Alban Lecocq <skeetmtp@gmail.com>
2004-05-20 14:39:09 +0000

    *** empty log message ***

--- mtp-target-web/stats.php
+++ mtp-target-web/stats.php
@@ -3,7 +3,7 @@
 <h3>Stats</h3>
 <ul>
 <li><a href="http://stats.mtp-target.org/">Web site stats</a></li>
-<li><a href="http://mtp-target.dyndns.org/cgi-bin/awstats.pl">login stats</a></li>
+<li><a href="http://mtp-target.dyndns.org/mtp-target/highscore.php">Score stats</a></li>
 <li><a href="http://cia.navi.cx/stats/project/mtp-target">CVS Stats</a></li>
 </ul>
 
