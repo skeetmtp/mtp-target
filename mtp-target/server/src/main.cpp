f90b491f6805d2a747eba51d00a037ab97ffc1cd
Vianney Lecroart <acemtp@gmail.com>
2004-05-21 12:10:09 +0000

    ADDED: log when server starts

--- mtp-target/server/src/main.cpp
+++ mtp-target/server/src/main.cpp
@@ -89,6 +89,21 @@ public:
 
 	void init()
 	{
+	  {
+	    FILE *fp = fopen("connection.stat", "ab");
+	    if(fp)
+	      {
+		char d[80];
+		time_t ltime;
+		time(&ltime);
+		struct tm *today = localtime(&ltime);
+		strftime(d, 80, "%04Y %02m %02d %02H %02M %02S", today);
+		fprintf(fp, "%u %s # Server restarted\n", ltime, d);
+		fclose(fp);
+	      }
+	  }
+	  
+
 		MainThreadId = getThreadId();
 #ifdef NL_OS_WINDOWS
 		BOOL res;
