8d0f673a365606f12547552479c9e4209051e080
Vianney Lecroart <acemtp@gmail.com>
2004-05-23 13:29:17 +0000

    *** empty log message ***

---
+++ mtp-target/make_tarball.sh
@@ -0,0 +1,20 @@
+#!/bin/sh -
+
+DEST=tmp/mtp-target/client
+rm -rf tmp
+mkdir tmp
+mkdir tmp/mtp-target
+mkdir $DEST
+cp -a client/src/client $DEST
+strip $DEST/client
+cp -a client/data $DEST
+cp -a client/cache $DEST
+cp -a client/mtp_target_default.cfg $DEST
+strip $HOME/install/release/lib/*.so
+cp -a $HOME/install/release/lib/*.so $DEST
+echo "#!/bin/sh -" >>$DEST/launch.sh
+echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:." >>$DEST/launch.sh
+echo "./client" >>$DEST/launch.sh
+chmod 700 $DEST/launch.sh
+cd tmp
+tar zcvf ../mtp-target-1.0.2.tgz mtp-target
