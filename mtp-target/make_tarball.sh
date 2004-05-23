1b7e0af799b98a8bbe8fa083d2166a0b0b602cf4
Vianney Lecroart <acemtp@gmail.com>
2004-05-23 14:33:28 +0000

    *** empty log message ***

--- mtp-target/make_tarball.sh
+++ mtp-target/make_tarball.sh
@@ -11,7 +11,14 @@ cp -a client/data $DEST
 cp -a client/cache $DEST
 cp -a client/mtp_target_default.cfg $DEST
 strip $HOME/install/release/lib/*.so
-cp -a $HOME/install/release/lib/*.so $DEST
+cp -a $HOME/install/release/lib/libnel3d.so.0.0.4 $DEST
+cp -a $HOME/install/release/lib/libnel3d.so.0 $DEST
+cp -a $HOME/install/release/lib/libnelnet.so.0.0.4 $DEST
+cp -a $HOME/install/release/lib/libnelnet.so.0 $DEST
+cp -a $HOME/install/release/lib/libnelmisc.so.0.0.4 $DEST
+cp -a $HOME/install/release/lib/libnelmisc.so.0 $DEST
+cp -a $HOME/install/release/lib/libnel_drv_opengl.so.0.0.0 $DEST
+cp -a $HOME/install/release/lib/libnel_drv_opengl.so.0 $DEST
 echo "#!/bin/sh -" >>$DEST/launch.sh
 echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:." >>$DEST/launch.sh
 echo "./client" >>$DEST/launch.sh
