e149fcc115b150a7e980a5f57567a6b5cf4bedd4
Alban Lecocq <skeetmtp@gmail.com>
2004-05-23 14:48:46 +0000

    ADDED : linux version

--- mtp-target-web/download.php
+++ mtp-target-web/download.php
@@ -23,7 +23,7 @@ echo "<h3>$menuLinkText_Download</h3>";
 ?>
 </tr></td>
 
-	<tr><td bgcolor="<?php echo $table_news_head_color;?>"><b>&nbsp;Full version&nbsp;</b></td></tr>
+	<tr><td bgcolor="<?php echo $table_news_head_color;?>"><b>&nbsp;Windows Full version&nbsp;</b></td></tr>
 	<?php
 
 		$dirname = "./download/";
@@ -40,7 +40,41 @@ echo "<h3>$menuLinkText_Download</h3>";
 			$t = filetype($f);
 			if($t == "file")
 			{
-				if(strstr($entry,"update")=="" && strstr($entry,"mtp-target")!="" )
+				if(strstr($entry,"update")=="" && strstr($entry,"mtp-target")!="" && strstr($entry,".exe")!="" )
+				{
+					printf("<tr>");
+					printf("<td bgcolor=\"%s\">&nbsp;<a href=\"$f\"><img src=\"download.png\" ALT=\"file\"> $entry</a>&nbsp;</td>",$table_news_row_color);
+					printf("</tr>");
+					$count++;
+				}
+			}
+		}
+		$d->close();
+		if($count==0)
+		{
+			printf("<tr>");
+			printf("<td bgcolor=\"%s\">No file available</td>",$table_news_row_color);
+			printf("</tr>");
+		}
+	?>
+	<tr><td bgcolor="<?php echo $table_news_head_color;?>"><b>&nbsp;Linux Full version&nbsp;</b></td></tr>
+	<?php
+
+		$dirname = "./download/";
+		$d = dir($dirname);
+		$count = 0;
+		while (false !== ($entry = $d->read())) 
+		{
+			if(($count%2)==0)
+				$color = $table_row_even_color;
+			else
+				$color = $table_row_odd_color;
+			
+			$f = $dirname . $entry ;
+			$t = filetype($f);
+			if($t == "file")
+			{
+				if(strstr($entry,"update")=="" && strstr($entry,"mtp-target")!="" && strstr($entry,".tgz")!="" )
 				{
 					printf("<tr>");
 					printf("<td bgcolor=\"%s\">&nbsp;<a href=\"$f\"><img src=\"download.png\" ALT=\"file\"> $entry</a>&nbsp;</td>",$table_news_row_color);
