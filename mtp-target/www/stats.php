dcdefe5560e04da0a16351b10d90be0fe9797470
Alban Lecocq <skeetmtp@gmail.com>
2004-05-26 20:45:30 +0000

    *** empty log message ***

--- mtp-target/www/stats.php
+++ mtp-target/www/stats.php
@@ -1,7 +1,6 @@
 <?php
 
-
-
+	$html_fp = fopen("stats.html", "wt");
 	$fp = fopen("../server/connection.stat", "r");
 	if (!$fp) {echo "<p>Unable to open remote file.</p>"; exit;}
 	
@@ -32,7 +31,7 @@
 
 		$name = str_replace("'","",$sname);
 
-		//printf("[%s] -> [%s]<br>",$sname,$name);
+		//fprintf($html_fp,"[%s] -> [%s]<br>",$sname,$name);
 		
 		$hour = intval($shour);
 		$minute = intval($smin);
@@ -44,16 +43,16 @@
 		{		
 			if($lastHour!=$hour)
 			{
-				//printf("%dh0 = %d<br>",$lastHour,$userCountPerHourTotal[$lastHour]);
+				//fprintf($html_fp,"%dh0 = %d<br>",$lastHour,$userCountPerHourTotal[$lastHour]);
 				$lastHour=$hour;
 				$userCountPerMinuteTodayMax = array_fill(0,60,-1);
 				$userCountPerMinuteTodayMax[0]=$playerCount;
 				/*
 				for($i=0;$i<60;$i++)
 				{
-					printf("%d<br>",$userCountPerMinuteTodayMax[$i]);
+					fprintf($html_fp,"%d<br>",$userCountPerMinuteTodayMax[$i]);
 				}
-				printf("<hr>");
+				fprintf($html_fp,"<hr>");
 				*/
 			}
 			
@@ -65,7 +64,7 @@
 					$userCountPerHourToday[$i]=0;
 					$userCountPerHourTodayMax[$i]=0;
 				}
-				//printf("<hr>");
+				//fprintf($html_fp,"<hr>");
 			}
 			
 			if($inout=='+')
@@ -78,7 +77,7 @@
 					$userCountPerHourTodayMax[$hour] = $playerCount;
 				if($playerCount>$userCountPerMinuteTodayMax[$minute])
 					$userCountPerMinuteTodayMax[$minute] = $playerCount;
-				//printf("%d:%d [%s] comes(%d,%d,%d) in<br>",$hour,$minute,$name,$playerCount,$userCountPerHourToday[$hour],$userCountPerMinuteTodayMax[$minute]);
+				//fprintf($html_fp,"%d:%d [%s] comes(%d,%d,%d) in<br>",$hour,$minute,$name,$playerCount,$userCountPerHourToday[$hour],$userCountPerMinuteTodayMax[$minute]);
 			}
 			else if($inout=='#')
 			{
@@ -89,19 +88,19 @@
 					$once = 0;
 				}
 				$playerCount = 0;
-				//printf("<hr>",$name);
+				//fprintf($html_fp,"<hr>",$name);
 			}
 			else if($inout=='?')
 			{
 				if(strlen($name)>0)
 					$playerCount++;
-				//printf("invalid login(%d) %s(%d)<br>",$playerCount,$name,strlen($name));
+				//fprintf($html_fp,"invalid login(%d) %s(%d)<br>",$playerCount,$name,strlen($name));
 			}
 			else if($inout=='-')
 			{
 				if(strlen($name)>0)
 					$playerCount--;
-				//printf("[%s](%d) leaves(%d)<br>",$name,strlen($name),$playerCount);
+				//fprintf($html_fp,"[%s](%d) leaves(%d)<br>",$name,strlen($name),$playerCount);
 			}
 		}
 		$fp++;
@@ -119,27 +118,24 @@
 	$upDay = ((($t3/60)/60)/24)%100;
 	
 	if($upDay == 1)
-		printf("Server uptime : %d day %02d:%02d:%02d <br>",$upDay,$upHour,$upMin,$upSec);
+		fprintf($html_fp,"Server uptime : %d day %02d:%02d:%02d <br>",$upDay,$upHour,$upMin,$upSec);
 	else if($upDay > 1)
-		printf("Server uptime : %d days %02d:%02d:%02d <br>",$upDay,$upHour,$upMin,$upSec);
+		fprintf($html_fp,"Server uptime : %d days %02d:%02d:%02d <br>",$upDay,$upHour,$upMin,$upSec);
 	else
-		printf("Server uptime : %02d:%02d:%02d <br>",$upHour,$upMin,$upSec);
-	printf("Current logged user : %d<br>\n",$playerCount);
-	printf("Last restart : %s<br>\n",date("l dS of F Y H:i:s",$lastRestartTime));
+		fprintf($html_fp,"Server uptime : %02d:%02d:%02d <br>",$upHour,$upMin,$upSec);
+	fprintf($html_fp,"Current logged user : %d<br>\n",$playerCount);
+	fprintf($html_fp,"Last restart : %s<br>\n",date("l dS of F Y H:i:s",$lastRestartTime));
 	
-?>
 
-<br>
-<br>
+fprintf($html_fp,"<br>\n");
+fprintf($html_fp,"<br>\n");
 
-<?php
-printf("<b>Total</b> login :<br>\n");
-printf("Since %s\n",date("l dS of F Y h:i:s A",$firstDate));
-?>
+fprintf($html_fp,"<b>Total</b> login :<br>\n");
+fprintf($html_fp,"Since %s\n",date("l dS of F Y h:i:s A",$firstDate));
 
-<table>
-<tr valign="bottom">
-<?php
+fprintf($html_fp,"<table>\n");
+fprintf($html_fp,"<tr valign=\"bottom\">\n");
+
 	$maxTotal = 0;
 	
 	for($i=0;$i<24;$i++)
@@ -150,45 +146,43 @@ printf("Since %s\n",date("l dS of F Y h:i:s A",$firstDate));
 	
 	for($i=0;$i<24;$i++)
 	{
-		//printf("=>%dh0 : %d<br>",$i,$userCountPerHourTotal[$i]);
-		printf("<td valign=\"bottom\">\n");
+		//fprintf($html_fp,"=>%dh0 : %d<br>",$i,$userCountPerHourTotal[$i]);
+		fprintf($html_fp,"<td valign=\"bottom\">\n");
 		if($maxTotal!=0)
-			printf("\t<img align=\"bottom\" src=\"./img/vp.png\" height=\"%d\" width=\"6\" alt='total login : %d' title='total login : %d' />\n",$userCountPerHourTotal[$i]*100/$maxTotal,$userCountPerHourTotal[$i],$userCountPerHourTotal[$i]);
-		printf("</td>\n");
+			fprintf($html_fp,"\t<img align=\"bottom\" src=\"./img/vp.png\" height=\"%d\" width=\"6\" alt='total login : %d' title='total login : %d' />\n",$userCountPerHourTotal[$i]*100/$maxTotal,$userCountPerHourTotal[$i],$userCountPerHourTotal[$i]);
+		ffprintf($html_fp,"</td>\n");
 	}
-	printf("</tr>\n");
+	fprintf($html_fp,"</tr>\n");
 	
-	printf("<tr>\n");
+	fprintf($html_fp,"<tr>\n");
 	for($i=0;$i<24;$i++)
 	{
-		printf("<td width=20>%d</td>\n",$userCountPerHourTotal[$i]);
+		fprintf($html_fp,"<td width=20>%d</td>\n",$userCountPerHourTotal[$i]);
 	}
-	printf("</tr>\n");	
+	fprintf($html_fp,"</tr>\n");	
 
-	printf("<tr>\n");
+	fprintf($html_fp,"<tr>\n");
 	for($i=0;$i<24;$i++)
 	{
-		printf("<td><img src=\"./img/hr%d.png\" width=\"10\"/></td>\n",($i%12)+1);
+		fprintf($html_fp,"<td><img src=\"./img/hr%d.png\" width=\"10\"/></td>\n",($i%12)+1);
 	}
-	printf("</tr>\n");	
+	fprintf($html_fp,"</tr>\n");	
 	
-	printf("<tr>\n");
+	fprintf($html_fp,"<tr>\n");
 	for($i=0;$i<24;$i++)
 	{
-		printf("<td>%d</td>\n",$i);
+		fprintf($html_fp,"<td>%d</td>\n",$i);
 	}
-	printf("</tr>\n");	
+	fprintf($html_fp,"</tr>\n");	
 
-?>
-</table>
+fprintf($html_fp,"</table>\n");
+fprintf($html_fp,"<br>\n");
+fprintf($html_fp,"<br>\n");
 
-<br>
-<br>
+fprintf($html_fp,"Today login :\n");
+fprintf($html_fp,"<table>\n");
+fprintf($html_fp,"<tr valign=\"bottom\">\n"
 
-Today login :
-<table>
-<tr valign="bottom">
-<?php
 	$maxToday = 0;
 	
 	for($i=0;$i<24;$i++)
@@ -200,45 +194,43 @@ Today login :
 	
 	for($i=0;$i<24;$i++)
 	{
-		printf("<td valign=\"bottom\">\n");
+		fprintf($html_fp,"<td valign=\"bottom\">\n");
 		if($maxToday!=0)
-			printf("\t<img align=\"bottom\" src=\"./img/vh.png\" height=\"%d\" width=\"6\" alt='today login : %d' title='today login : %d' />\n",$userCountPerHourToday[$i]*100/$maxToday,$userCountPerHourToday[$i],$userCountPerHourToday[$i]);
-		printf("</td>\n");
+			fprintf($html_fp,"\t<img align=\"bottom\" src=\"./img/vh.png\" height=\"%d\" width=\"6\" alt='today login : %d' title='today login : %d' />\n",$userCountPerHourToday[$i]*100/$maxToday,$userCountPerHourToday[$i],$userCountPerHourToday[$i]);
+		fprintf($html_fp,"</td>\n");
 	}
-	printf("</tr>\n");
+	fprintf($html_fp,"</tr>\n");
 	
-	printf("<tr>\n");
+	fprintf($html_fp,"<tr>\n");
 	for($i=0;$i<24;$i++)
 	{
-		printf("<td width=20>%d</td>\n",$userCountPerHourToday[$i]);
+		fprintf($html_fp,"<td width=20>%d</td>\n",$userCountPerHourToday[$i]);
 	}
-	printf("</tr>\n");	
+	fprintf($html_fp,"</tr>\n");	
 	
-	printf("<tr>\n");
+	fprintf($html_fp,"<tr>\n");
 	for($i=0;$i<24;$i++)
 	{
-		printf("<td><img src=\"./img/hr%d.png\" width=\"10\"/></td>\n",($i%12)+1);
+		fprintf($html_fp,"<td><img src=\"./img/hr%d.png\" width=\"10\"/></td>\n",($i%12)+1);
 	}
-	printf("</tr>\n");	
+	fprintf($html_fp,"</tr>\n");	
 
-	printf("<tr>\n");
+	fprintf($html_fp,"<tr>\n");
 	for($i=0;$i<24;$i++)
 	{
-		printf("<td>%d</td>\n",$i);
+		fprintf($html_fp,"<td>%d</td>\n",$i);
 	}
-	printf("</tr>\n");	
+	fprintf($html_fp,"</tr>\n");	
 	
-?>
-</table>
-
-<br>
-<br>
+fprintf($html_fp,"</table>\n");
+fprintf($html_fp,"<br>\n");
+fprintf($html_fp,"<br>\n");
 
 
-Today Max simultaneous user :
-<table>
-<tr valign="bottom">
-<?php
+fprintf($html_fp,"Today Max simultaneous user :\n");
+fprintf($html_fp,"<table>\n");
+fprintf($html_fp,"<tr valign="bottom">\n");
+
 	$maxTodayMax = 0;
 	
 	for($i=0;$i<24;$i++)
@@ -250,44 +242,42 @@ Today Max simultaneous user :
 	
 	for($i=0;$i<24;$i++)
 	{
-		printf("<td valign=\"bottom\">\n");
+		fprintf($html_fp,"<td valign=\"bottom\">\n");
 		if($maxTodayMax!=0)
-			printf("\t<img align=\"bottom\" src=\"./img/vh.png\" height=\"%d\" width=\"6\" alt='today simultaneous user : %d' title='today simultaneous user : %d' />\n",$userCountPerHourTodayMax[$i]*100/$maxTodayMax,$userCountPerHourTodayMax[$i],$userCountPerHourTodayMax[$i]);
-		printf("</td>\n");
+			fprintf($html_fp,"\t<img align=\"bottom\" src=\"./img/vh.png\" height=\"%d\" width=\"6\" alt='today simultaneous user : %d' title='today simultaneous user : %d' />\n",$userCountPerHourTodayMax[$i]*100/$maxTodayMax,$userCountPerHourTodayMax[$i],$userCountPerHourTodayMax[$i]);
+		fprintf($html_fp,"</td>\n");
 	}
-	printf("</tr>\n");
+	fprintf($html_fp,"</tr>\n");
 	
-	printf("<tr>\n");
+	fprintf($html_fp,"<tr>\n");
 	for($i=0;$i<24;$i++)
 	{
-		printf("<td width=20>%d</td>\n",$userCountPerHourTodayMax[$i]);
+		fprintf($html_fp,"<td width=20>%d</td>\n",$userCountPerHourTodayMax[$i]);
 	}
-	printf("</tr>\n");	
+	fprintf($html_fp,"</tr>\n");	
 	
-	printf("<tr>\n");
+	fprintf($html_fp,"<tr>\n");
 	for($i=0;$i<24;$i++)
 	{
-		printf("<td><img src=\"./img/hr%d.png\" width=\"10\"/></td>\n",($i%12)+1);
+		fprintf($html_fp,"<td><img src=\"./img/hr%d.png\" width=\"10\"/></td>\n",($i%12)+1);
 	}
-	printf("</tr>\n");	
+	fprintf($html_fp,"</tr>\n");	
 
-	printf("<tr>\n");
+	fprintf($html_fp,"<tr>\n");
 	for($i=0;$i<24;$i++)
 	{
-		printf("<td>%d</td>\n",$i);
+		fprintf($html_fp,"<td>%d</td>\n",$i);
 	}
-	printf("</tr>\n");	
+	fprintf($html_fp,"</tr>\n");	
 	
-?>
-</table>
+fprintf($html_fp,"</table>\n");
+fprintf($html_fp,"<br>\n");
+fprintf($html_fp,"<br>\n");
 
-<br>
-<br>
+fprintf($html_fp,"Last hour max simultaneous user :\n");
+fprintf($html_fp,"<table>\n");
+fprintf($html_fp,"<tr valign=\"bottom\">\n");
 
-Last hour max simultaneous user :
-<table>
-<tr valign="bottom">
-<?php
 	$maxHourMax = 0;
 	
 	for($i=0;$i<60 && $i<$lastMinute;$i++)
@@ -301,37 +291,37 @@ Last hour max simultaneous user :
 	
 	for($i=0;$i<60;$i++)
 	{
-		printf("<td valign=\"bottom\">\n");
+		fprintf($html_fp,"<td valign=\"bottom\">\n");
 		if($maxHourMax!=0)
-			printf("\t<img align=\"bottom\" src=\"./img/vp.png\" height=\"%d\" width=\"6\" alt='today simultaneous user : %d' title='today simultaneous user : %d' />\n",$userCountPerMinuteTodayMax[$i]*100/$maxHourMax,$userCountPerMinuteTodayMax[$i],$userCountPerMinuteTodayMax[$i]);
-		printf("</td>\n");
+			fprintf($html_fp,"\t<img align=\"bottom\" src=\"./img/vp.png\" height=\"%d\" width=\"6\" alt='today simultaneous user : %d' title='today simultaneous user : %d' />\n",$userCountPerMinuteTodayMax[$i]*100/$maxHourMax,$userCountPerMinuteTodayMax[$i],$userCountPerMinuteTodayMax[$i]);
+		fprintf($html_fp,"</td>\n");
 	}
-	printf("</tr>\n");
+	fprintf($html_fp,"</tr>\n");
 	
 	
-	printf("<tr>\n");
+	fprintf($html_fp,"<tr>\n");
 	for($i=0;$i<60 && $i<=$lastMinute;$i++)
 	{
-		printf("<td>%d</td>\n",$userCountPerMinuteTodayMax[$i]);
+		fprintf($html_fp,"<td>%d</td>\n",$userCountPerMinuteTodayMax[$i]);
 	}
-	printf("</tr>\n");	
+	fprintf($html_fp,"</tr>\n");	
 	
-	printf("<tr>\n");
+	fprintf($html_fp,"<tr>\n");
 	for($i=0;$i<60 && $i<=$lastMinute;$i++)
 	{
-		printf("<td>%02d</td>\n",$i);
+		fprintf($html_fp,"<td>%02d</td>\n",$i);
 	}
-	printf("</tr>\n");	
+	fprintf($html_fp,"</tr>\n");	
 	
 	
 	/*
-	printf("<tr>\n");
+	fprintf($html_fp,"<tr>\n");
 	for($i=0;$i<24;$i++)
 	{
-		printf("<td><img src=\"./img/hr%d.png\" width=\"10\"/></td>\n",($i%12)+1);
+		fprintf($html_fp,"<td><img src=\"./img/hr%d.png\" width=\"10\"/></td>\n",($i%12)+1);
 	}
-	printf("</tr>\n");
+	fprintf($html_fp,"</tr>\n");
 	*/
+fprintf($html_fp,"</table>\n");
+	fclose($html_fp);
 ?>
-</table>
-
