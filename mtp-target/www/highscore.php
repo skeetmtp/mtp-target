66ac7c2e6b03fb0aa9b0ca23531aed3904f07a06
Vianney Lecroart <acemtp@gmail.com>
2004-05-29 12:37:46 +0000

    ADDE: team support

--- mtp-target/www/highscore.php
+++ mtp-target/www/highscore.php
@@ -39,20 +39,33 @@
 			list($user,$pass,$score) = explode(" ",$line);
 			$arr = explode(" ",$line);
 			$p=0;
+			$teamnb = 0;
 
 			for ($i = 0; $i+2 < sizeof($arr);)
 			{
 				$my1[$p] = strtr($arr[$i],"\""," ");
 				$my2[$p] = (int)(strtr($arr[$i+2],"\""," "));
+
+				$b = strpos($my1[$p],'[');
+				$e = strpos($my1[$p],']');
+				if($b !== false && $e !== false)
+				{
+					$team = substr($my1[$p],$b+1,$e-$b-1);
+					$team_name[$teamnb] = $team;
+					$team_score[$teamnb] = $team_score[$teamnb] + $my2[$p];
+					$teamnb++;
+				}
+
 				$i+=3;
 				$p++;
 			}
 			array_multisort($my2, SORT_DESC, $my1);
+			array_multisort($team_score, SORT_DESC, $team_name);
 
 			echo "<h3>Registered:</h3>";
 			echo sizeof($my1)." players</p>";
 
-			echo "<h3>Highscores:</h3>";
+			echo "<h3>Player Highscores:</h3>";
 			for ($i = 0; $i < 1; $i++)
 				echo "<font style=\"color:red\"><b>$my1[$i]</b> : $my2[$i]</font><br>";
 
@@ -61,6 +74,11 @@
 
 			for (; $i < 10; $i++)
 				echo "$my1[$i] : $my2[$i]<br>";
+
+                        echo "<h3>Team Highscores:</h3>";
+                        for ($i = 0; $i < 3; $i++)
+                                echo "$team_name[$i] : $team_score[$i]<br>";
+
 		}
 		else if (strstr ($line, "Levels"))
 		{
