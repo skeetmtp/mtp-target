81a152322892ccb50c09a4481f07609471ec7d7d
Alban Lecocq <skeetmtp@gmail.com>
2004-05-29 15:03:53 +0000

    *** empty log message ***

--- mtp-target/server/data/level/level_race.lua
+++ mtp-target/server/data/level/level_race.lua
@@ -69,6 +69,5 @@ Modules =
 
 Particles =
 {
---	{ Position = CVector(0.000000,0.000000,0.790971), Scale = CVector(10.000000, 30.000000, 10.000000), Rotation = CAngleAxis(0.000000,0.000000,1.000000,0.000000), Name = "toto", FileName = "logo" },
 }
 
