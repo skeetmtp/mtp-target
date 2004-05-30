a2ec5796c14c83e1a513360a273c28012670be39
Alban Lecocq <skeetmtp@gmail.com>
2004-05-30 14:34:10 +0000

    *** empty log message ***

--- mtp-target-web/download.php
+++ mtp-target-web/download.php
@@ -162,41 +162,6 @@ echo "<h3>$menuLinkText_Download</h3>";
 		}
 	?>
 	<tr><td><b>&nbsp;&nbsp;</b></td></tr>
-	<tr><td bgcolor="<?php echo $table_news_head_color;?>"><b>&nbsp;Medias&nbsp;</b></td></tr>
-	<?php
-
-		$dirname = "./media/";
-		$d = dir($dirname);
-		$count = 0;
-		while (false !== ($entry = $d->read())) 
-		{
-			if(($count%2)==0)
-				$color = $table_row_even_color;
-			else
-				$color = $table_row_odd_color;
-			
-			$f = $dirname . $entry ;
-			$t = filetype($f);
-			if($t == "file")
-			{
-				//if(strstr($entry,"update")=="" && strstr($entry,"mtp-target")==""  && strstr($entry,"index")=="" )
-				{
-					printf("<tr>");
-					printf("<td bgcolor=\"%s\">&nbsp;<a href=\"$f\"><img src=\"download.png\" ALT=\"file\"> $entry</a>&nbsp;</td>",$table_news_row_color);
-					printf("</tr>");
-					$count++;
-				}
-			}
-		}
-		$d->close();
-		if($count==0)
-		{
-			printf("<tr>");
-			printf("<td bgcolor=\"%s\">No file available</td>",$table_news_row_color);
-			printf("</tr>");
-		}
-	?>
-	<tr><td><b>&nbsp;&nbsp;</b></td></tr>
 </table>
 
 
