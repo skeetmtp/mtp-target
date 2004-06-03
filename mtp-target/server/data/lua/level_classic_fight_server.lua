06b9124d30a194eeb3738883469258e30e20c8eb
Alban Lecocq <skeetmtp@gmail.com>
2004-06-03 18:45:56 +0000

    ADDED : warning message in fight level

--- mtp-target/server/data/lua/level_classic_fight_server.lua
+++ mtp-target/server/data/lua/level_classic_fight_server.lua
@@ -6,10 +6,9 @@
 local clientId = 0;
 
 function Entity:init()
+  self:displayText(0,5,1,255,200,0,"warning : In this level you can control pingoo on target",60);
   self:setCurrentScore(0);
   self:setOpenCloseMax(3);
-  --print(self:getName());
-  --print("entity init");
   self:setStartPointId(clientId+getSessionId()*2);
   clientId = clientId + 1;
 end
@@ -22,25 +21,13 @@ function Entity:update()
 end
 
 function entitySceneCollideEvent ( entity, module )
-  --print(entity:getName());
-  --print(module:getName());
-  --print("entitySceneCollideEvent");
-  --print(entity:getName());
-  --print(module:getScore());
-  
   module:collide(entity);
-  --entity:setCurrentScore(module:getScore());
 end
 
 function entityEntityCollideEvent ( entity1, entity2 )
-  --print(entity1:getName());
-  --print(entity2:getName());
-  --print("entityEntityCollideEvent");
 end
 
 function entityWaterCollideEvent ( entity )
-  --print(entity:getName());
-  --print("entityWaterCollideEvent");
 end
 
 function Module:collide( entity )
