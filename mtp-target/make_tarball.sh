ff164339fedd4fe0207a109ff5183b5cec113cd5
Vianney Lecroart <acemtp@gmail.com>
2004-06-08 09:12:34 +0000

    ADDED: generate client and server package

--- mtp-target/make_tarball.sh
+++ mtp-target/make_tarball.sh
@@ -2,30 +2,53 @@
 
 VERSION=1.0.4
 
-DEST=tmp/mtp-target/client
+DEST=tmp/mtp-target
+CDEST=$DEST/client
+SDEST=$DEST/server
+
+# create directories
 rm -rf tmp
 mkdir tmp
-mkdir tmp/mtp-target
 mkdir $DEST
-cp -a client/src/client $DEST
-strip $DEST/client
-cp -a client/data $DEST
-cp -a client/cache $DEST
-cp -a client/mtp_target_default.cfg $DEST
+mkdir $CDEST
+mkdir $SDEST
+mkdir $DEST/lib
+
+# copy client files
+cp -a client/src/client $CDEST
+strip $CDEST/client
+cp -a client/data $CDEST
+cp -a client/cache $CDEST
+cp -a client/mtp_target_default.cfg $CDEST
+echo "#!/bin/sh -" >>$CDEST/launch.sh
+echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:../lib" >>$CDEST/launch.sh
+echo "./client" >>$CDEST/launch.sh
+chmod 700 $CDEST/launch.sh
+
+# copy server files
+cp -a server/src/mtp_target_service $SDEST
+strip $SDEST/mtp_target_service
+cp -a server/data $SDEST
+cp -a server/mtp_target_service_default.cfg $SDEST
+echo "#!/bin/sh -" >>$SDEST/launch.sh
+echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:../lib" >>$SDEST/launch.sh
+echo "./mtp_target_service" >>$SDEST/launch.sh
+chmod 700 $SDEST/launch.sh
+
+# copy libs
 strip $HOME/install/release/lib/*.so
-cp -a $HOME/install/release/lib/libnel3d.so.0.0.4 $DEST
-cp -a $HOME/install/release/lib/libnel3d.so.0 $DEST
-cp -a $HOME/install/release/lib/libnelnet.so.0.0.4 $DEST
-cp -a $HOME/install/release/lib/libnelnet.so.0 $DEST
-cp -a $HOME/install/release/lib/libnelmisc.so.0.0.4 $DEST
-cp -a $HOME/install/release/lib/libnelmisc.so.0 $DEST
-cp -a $HOME/install/release/lib/libnel_drv_opengl.so.0.0.0 $DEST
-cp -a $HOME/install/release/lib/libnel_drv_opengl.so.0 $DEST
-echo "#!/bin/sh -" >>$DEST/launch.sh
-echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:." >>$DEST/launch.sh
-echo "./client" >>$DEST/launch.sh
-chmod 700 $DEST/launch.sh
+cp -a $HOME/install/release/lib/libnel3d.so.0.0.4 $DEST/lib
+cp -a $HOME/install/release/lib/libnel3d.so.0 $DEST/lib
+cp -a $HOME/install/release/lib/libnelnet.so.0.0.4 $DEST/lib
+cp -a $HOME/install/release/lib/libnelnet.so.0 $DEST/lib
+cp -a $HOME/install/release/lib/libnelmisc.so.0.0.4 $DEST/lib
+cp -a $HOME/install/release/lib/libnelmisc.so.0 $DEST/lib
+cp -a $HOME/install/release/lib/libnel_drv_opengl.so.0.0.0 $DEST/lib
+cp -a $HOME/install/release/lib/libnel_drv_opengl.so.0 $DEST/lib
+
+# tar and send
+rm -f mtp-target-*.tgz
 cd tmp
-rm -f ../mtp-target-*.tgz
-tar zcvf ../mtp-target-$VERSION.tgz mtp-target
+tar jcvf ../mtp-target-$VERSION.tar.bz2 mtp-target
+cd ..
 ftp ftpperso.free.fr
