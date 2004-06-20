8a0742bbb6a8f34165d1e2e7b0a82c6e76f36cdf
Vianney Lecroart <acemtp@gmail.com>
2004-06-20 18:41:58 +0000

    *** empty log message ***

--- mtp-target/make_tarball.sh
+++ mtp-target/make_tarball.sh
@@ -2,6 +2,8 @@
 
 VERSION=1.1.0
 
+TAGVERSION=`echo $VERSION | tr \. _`
+
 DEST=tmp/mtp-target
 CDEST=$DEST/client
 SDEST=$DEST/server
@@ -21,7 +23,7 @@ cp -a client/data $CDEST
 cp -a client/cache $CDEST
 cp -a client/mtp_target_default.cfg $CDEST
 echo "#!/bin/sh -" >>$CDEST/launch.sh
-echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:../lib" >>$CDEST/launch.sh
+echo "export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:../lib" >>$CDEST/launch.sh
 echo "./client" >>$CDEST/launch.sh
 chmod 700 $CDEST/launch.sh
 
@@ -31,7 +33,7 @@ strip $SDEST/mtp_target_service
 cp -a server/data $SDEST
 cp -a server/mtp_target_service_default.cfg $SDEST
 echo "#!/bin/sh -" >>$SDEST/launch.sh
-echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:../lib" >>$SDEST/launch.sh
+echo "export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:../lib" >>$SDEST/launch.sh
 echo "./mtp_target_service" >>$SDEST/launch.sh
 chmod 700 $SDEST/launch.sh
 
@@ -46,8 +48,18 @@ cp -a $HOME/install/release/lib/libnelmisc.so.0 $DEST/lib
 cp -a $HOME/install/release/lib/libnel_drv_opengl.so.0.0.0 $DEST/lib
 cp -a $HOME/install/release/lib/libnel_drv_opengl.so.0 $DEST/lib
 
+cp /usr/lib/libfreetype.so.6 $DEST/lib
+cp /usr/lib/liblua50.so.5.0 $DEST/lib
+cp /usr/lib/libxml2.so.2 $DEST/lib
+cp /usr/lib/libz.so.1 $DEST/lib
+cp /usr/lib/libstlport_gcc.so.4.6 $DEST/lib
+cp /usr/lib/libjpeg.so.62 $DEST/lib
+cp /lib/libcrypt.so.1 $DEST/lib
+
+cvs -z3 -q tag -F release_$TAGVERSION
+
 # tar and send
-rm -f mtp-target-*.tgz
+rm -f mtp-target-*.tar.bz2
 cd tmp
 tar jcvf ../mtp-target-$VERSION.tar.bz2 mtp-target
 cd ..
