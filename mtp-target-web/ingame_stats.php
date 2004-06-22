030dbab28d974f1e10a13df729329ff205aae9c1
Alban Lecocq <skeetmtp@gmail.com>
2004-06-22 20:36:18 +0000

    *** empty log message ***

--- mtp-target-web/ingame_stats.php
+++ mtp-target-web/ingame_stats.php
@@ -1,26 +1,34 @@
 <?php 
+	include_once("config.php");
+	
 	function getStats(&$NbOnlinePlayers, &$NbRegisteredPlayers, &$NbSessions)
 	{
-		$DBHost = "mtp-target.dyndns.org";
-		$DBUserName = "www";
-		$DBPassword = "www";
-		$DBName = "nel";
-
-		$link = mysql_connect($DBHost, $DBUserName, $DBPassword) or die ("Can't connect to database host:$DBHost user:$DBUserName");
-		mysql_select_db ($DBName) or die ("Can't access to the table dbname:$DBName");
 		$query = "select count(*) from user where State='Online'";
-		$result = mysql_query ($query) or die ("Can't execute the query: ".$query);
+		$result = exec_game_db_requete($query);
 		$row = mysql_fetch_row ($result);
 		$NbOnlinePlayers = $row[0];
 
 		$query = "select count(*) from user";
-		$result = mysql_query ($query) or die ("Can't execute the query: ".$query);
+		$result = exec_game_db_requete($query);
 		$row = mysql_fetch_row ($result);
 		$NbRegisteredPlayers = $row[0];
 
 		$query = "select count(*) from session";
-		$result = mysql_query ($query) or die ("Can't execute the query: ".$query);
+		$result = exec_game_db_requete($query);
 		$row = mysql_fetch_row ($result);
 		$NbSessions = $row[0];
 	}
+	
+	function getUserTotalDuration($uid,&$totalDurationStr)
+	{
+		$uid_res = exec_game_db_requete(sprintf("select SUM(Duration) from user_session WHERE UId=%d",$uid));
+		$uid_array = mysql_fetch_row ($uid_res);
+		$total_time = $uid_array[0];
+		$total_sec = ($total_time)%60;
+		$total_min = ($total_time/60)%60;
+		$total_hour = ($total_time/60/60)%24;
+		$totalDurationStr = sprintf("%02d:%02d:%02d",$total_hour,$total_min,$total_sec);
+		return $total_time;
+	}
+	
 ?>
