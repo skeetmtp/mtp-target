192bab9f0b6e8a5d520b7f4620ca38ebdaedd483
Alban Lecocq <skeetmtp@gmail.com>
2004-06-23 15:57:11 +0000

    CHANGE : release level

--- mtp-target/server/data/level/level_the_lane.lua
+++ mtp-target/server/data/level/level_the_lane.lua
@@ -1,7 +1,7 @@
 Name = "The Lan"
 Author = "Skeet"
 ServerLua = "level_default_server.lua"
-ReleaseLevel = 4
+ReleaseLevel = 2
 
 skyShapeFileName = "sky.shape";
 sunAmbientColor = CRGBA(82, 100, 133, 255);
