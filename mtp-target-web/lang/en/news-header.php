95562cbcb49ef9fada1586623cda706c80b0f029
Alban Lecocq <skeetmtp@gmail.com>
2004-06-23 12:51:51 +0000

    *** empty log message ***

--- mtp-target-web/lang/en/news-header.php
+++ mtp-target-web/lang/en/news-header.php
@@ -6,8 +6,10 @@
 <b>Server :</b><br>
 If you are administrator of a GNU/Linux or Windows(c) server, you can help us, by hosting a mtp-target server.
 The program needs few CPU, bandwith and memory (about 3MB of ram and 1 MB on HDD)<br>
+<!--
 <b>WWW :</b><br>
 We also need a php/sql/html webmaster to make more stat page and user settings page.<br>
+-->
 <b>Level :</b><br>
 If you can make meshes/texture for level elements<br>
 If you have any idea or just want to help us.<br>
--- mtp-target-web/lang/fr/news-header.php
+++ mtp-target-web/lang/fr/news-header.php
@@ -6,8 +6,10 @@
 <b>Serveur :</b><br>
 Si vous etes administrateur d'un serveur GNU/Linux ou Windows(tm), vous pouvez nous aider, en hebergeant un serveur.
 Ce programme necessite peu de CPU, peu de bande passante et peu de memoire (à peu pres 3Mo de RAM et 1Mo sur le dur)<br>
+<!--
 <b>WWW :</b><br>
 Nous avons aussi besoin d'un webmaster php/sql/html pour faire plus de page de stats et les page des utilisateur.<br>
+-->
 <b>Niveau :</b><br>
 Si vous pouvez faire des meshes/texture pour les elements de niveau<br>
 Si vous avez des idees ou que vous voulez juste nous aider<br>
