a8e832ccbb04f498f9395c731c2d51ae994ada36
Vianney Lecroart <acemtp@gmail.com>
2004-06-27 18:49:29 +0000

    ADDED: old score update

--- mtp-target/www/dump.php
+++ mtp-target/www/dump.php
@@ -1,9 +1,9 @@
+<?php
+
 # This script is used to insert all users that is not on the database
 # easy way to add the score if necessary
 
-<?php
-
-    $filename = "/home/ace/cvs/mtp-target/server/mtp_target_service.cfg";
+    $filename = "/home/ace/cvs/mtp-target/adv_server/mtp_target_service.cfg";
 
 	$fp = fopen ($filename, "rb");
 	if (!$fp)
@@ -13,8 +13,8 @@
 	}
 
 	$DBHost = "mtp-target.dyndns.org";
-	$DBUserName = "";
-	$DBPassword = "";
+	$DBUserName = "root";
+	$DBPassword = "PlokiAqz";
 	$DBName = "nel";
 
 	$link = mysql_connect($DBHost, $DBUserName, $DBPassword) or die ("Can't connect to database host:$DBHost user:$DBUserName");
@@ -46,12 +46,16 @@
 
 				$pass = trim($arr[$i+1]);
 				$pass = trim($pass, "\"");
+
 				$score = (int)(strtr($arr[$i+2],"\""," "));
 
-				$query = "select * from user where Login='".$login."'";
+				//echo $login." ".$score."<br>";
+
+				//$query = "select * from user where Login='".$login."'";
+				//$query = "update user set Score=Score+".$score." where Login='".$login."'";
 				//echo $query."<br>";
 				$result = mysql_query ($query) or die ("Can't execute the query: ".$query);
-				$num = mysql_num_rows($result);
+/*				$num = mysql_num_rows($result);
 				if($num != 1)
 				{
 					$cpass = crypt($pass, "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789./");
@@ -64,11 +68,12 @@
 
 					$nb++;
 				}
+*/
 				$i+=3;
 			}
 		}
 	}
 	fclose($fp);
 
-	echo "Number of row affected: ".$nb;
+//	echo "Number of row affected: ".$nb;
 ?>
