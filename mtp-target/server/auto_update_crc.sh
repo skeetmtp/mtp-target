0b88fc1327f61ed1bf3c4ab473fcc6e173f09efb
Alban Lecocq <skeetmtp@gmail.com>
2004-06-28 22:39:02 +0000

    *** empty log message ***

--- mtp-target/server/auto_update_crc.sh
+++ mtp-target/server/auto_update_crc.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-if [ -e ../user_texture/update_crc_flag.txt ]
+if [ -e /home/ace/cvs/mtp-target/user_texture/update_crc_flag.txt ]
 then
    echo "crc update";
    wget -O update_crc.log http://mtp-target.dyndns.org/mtp-target/crc.php
-   rm -f ../user_texture/update_crc_flag.txt
+   rm -f /home/ace/cvs/mtp-target/user_texture/update_crc_flag.txt
 else
    echo "no crc update";
 fi
