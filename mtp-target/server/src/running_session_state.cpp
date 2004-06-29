7877ed83240c8235d101cde1f74de640f4245ed0
Alban Lecocq <skeetmtp@gmail.com>
2004-06-29 12:34:20 +0000

    FIXED : can't break record if time < 10 sec (timebeforecheck)

--- mtp-target/server/src/running_session_state.cpp
+++ mtp-target/server/src/running_session_state.cpp
@@ -79,7 +79,8 @@ void CRunningSessionState::update()
 					}
 					else //si il est arrete
 					{
-						if(c->ArrivalTime == 0 && c->CurrentScore>0)
+						float minArrivalTime = TimeBeforeCheck/1000.0f + 1.0f;
+						if(c->ArrivalTime > minArrivalTime  && c->CurrentScore>0)
 						{
 							c->ArrivalTime =(float)(currentTime - CSessionManager::instance().startTime())/1000.0f;
 							nlinfo("*** ARRIVAL TIME '%s' %f",c->name().c_str(),c->ArrivalTime);
