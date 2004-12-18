f04dc3bf300d7783fec679b97166228658ae7e4e
Alban Lecocq <skeetmtp@gmail.com>
2004-12-18 21:41:58 +0000

    ADDED : total score in user welcome

--- mtp-target-web/user_welcome.php
+++ mtp-target-web/user_welcome.php
@@ -7,6 +7,7 @@
  <?php
    printf("Welcome <b>%s</b>... (<a href=\"user_logout.php\">logout</a>)<br>",$user_login);
    printf("Today score : %d<br>",userTodayScore($user_id));
+   printf("Total score : %d<br>",userCurrentScore($user_id));
    printf("<b>");
    printf("<a href=\"?page=user_settings.php\">your settings</a><br>");
    printf("<a href=\"?page=stat_user.php&p_user_id=%d\">your stats</a><br>",$user_id);
