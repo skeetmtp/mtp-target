3d027c15f3d0da7485931f3682f4020e0e8436a0
Vianney Lecroart <acemtp@gmail.com>
2005-03-24 16:40:25 +0000

    ADDED: donate

---
+++ mtp-target-web/donate.php
@@ -0,0 +1,214 @@
+<?php
+
+     switch($lang)
+     {
+         case "fr":
+?>
+
+<h3>Introduction</h3>
+
+<p>Cela fait maintenant plusieurs années que le projet Mtp Target existe et de
+nouveaux joueurs arrivent tous les jours. Ce jeu a toujours été gratuit et cela
+ne permet plus aujourd'hui d'avoir une qualité de jeu suffisante. Jusqu'à lors,
+Nous avions toujours réussi à trouver des serveurs gratuits pour stoquer nos
+bases de données, les serveurs de jeux, les serveurs pour les pages web mais
+cela entraine de nombreux desagrement. Par exemple, dû à un serveur qui
+nous à été repris, nous avons perdu une partie des scores. Le serveur de jeu est
+actuellement hébergé chez un particulier avec une ligne ADSL qui est partagé
+avec d'autres activités et qui rend le jeu très instable (quand il n'est pas
+tout simplement inaccessible).</p>
+
+<p>Pour améliorer le jeu, il nous faut nos propres serveurs dédiés, notre
+propre bande passante et pour cela il faut... de l'argent! Nous avons eu
+plusieurs possibilités et celle qui a été retenue pour le moment est de garder
+le jeu entièrement libre et gratuit mais de faire appel à votre bonne volonté.</p>
+
+<p>Alors si vous aimez ce jeu et que vous voulez le soutenir, faite un don! Tout
+l'argent sera utilisé pour l'hebergement des serveurs et de ce fait améliorer
+la qualité du jeu. Si nous arrivons pas à obtenir suffisamment d'argent, il est
+très probable que le jeu ne s'ameliore jamais, voir même que le jeu disparraisse :(</p>
+
+<p>Vous êtes libre de choisir la somme que vous voulez donner, nous avons fixé
+des seuils qui vous permettent d'être reconnu comme aidant activement Mtp Target.
+Si vous donnez 15 Euros ou plus, votre nom sera affiché dans le tableau des
+contributeurs (sauf si vous demandez de rester anonyme). Bien sûr, les dons
+inferieurs à 15 euros sont aussi les bienvenus.</p>
+
+<?php boxBegin(false); ?>
+15 Euros</td><td>Supporter</td></tr><tr><td>
+30 Euros</td><td>Contributeur</td></tr><tr><td>
+60 Euros</td><td>Sponsors</td></tr><tr><td>
+Au dela</td><td>des titres spécifiques vous seront attribués
+<?php boxEnd(); ?>
+
+<p>Voiçi les differents systèmes disponiblent pour faire un don:</p>
+
+<h3>PayPal</h3>
+
+<p>C'est la méthode qui vous permet de payer via Internet Sécurisé. Vous avez
+juste besoin d'une carte Visa ou Mastercard ou un compte PayPal (si vous
+n'en avez pas, vous pouvez quand même payer sans devoir vous inscrire).</p>
+
+<p>Attention, si vous utilisez ce système, une commission est prise par PayPal
+(0.XXEuro + X.XX%) sur le montant que vous mettez.</p>
+
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+<input type="hidden" name="cmd" value="_xclick">
+<input type="hidden" name="business" value="acemtp@gmail.com">
+<input type="hidden" name="item_name" value="Dons Mtp Target">
+<input type="hidden" name="no_shipping" value="1">
+<input type="hidden" name="no_note" value="1">
+<input type="hidden" name="currency_code" value="EUR">
+<input type="hidden" name="tax" value="0">
+<input type="hidden" name="lc" value="FR">
+Valeur du don:<br>
+<input type="text" name="amount" size="2" value="15"> Euros<br/>
+<input type="image" src="https://www.paypal.com/fr_FR/i/btn/x-click-but04.gif" border="0" name="submit" alt="Faire un don via PayPal">
+</form>
+
+<h3>Virement bancaire</h3>
+
+<p>Le virement bancaire vous permet d'être sûr que tout l'argent que vous
+donnez nous revient. Voici le compte à créditer:</p>
+
+<p>RIB (uniquement pour la France):
+<?php boxBegin(false); ?>
+Banque</td><td>Guichet</td><td>Numéro de compte</td><td>Clé</td>
+</tr><tr>
+<td>12548</td><td>02998</td><td>150175 115 03</td><td>61
+<?php boxEnd(); ?>
+</p>
+
+<p>IBAN (utilisation internationale):
+<?php boxBegin(false); ?>
+FR76 1254 8029 9815 0175 1150 361</td>
+</tr><tr>
+<td>Bank Indentification Code: AXABFRPP
+<?php boxEnd(); ?>
+</p>
+
+<h3>Chèque (uniquement pour la France)</h3>
+
+<p>Vous pouvez aussi tout simplement envoyer un chèque à l'ordre de Vianney
+Lecroart à l'adresse suivante:
+<?php boxBegin(false); ?>
+Lecroart Vianney</td></tr>
+<tr><td>Dons pour Mtp Target</td></tr>
+<tr><td>21 rue planchat</td></tr>
+<tr><td>75020 Paris</td></tr>
+<?php boxEnd(); ?>
+</p>
+
+<h3>Hall des supporters</h3>
+<table>
+<tr><td>Nom</td><td>Dons</td></tr>
+
+<?php
+         break;
+         default :
+?>
+
+<h3>Introduction</h3>
+
+<p>Mtp Target already exists for a few years and there are new players everydays.
+This game has always been free (as free beer and as freedom) and now it's not
+possible to continue this way due to the bad game quality. We always found free
+servers to host our database, game servers, web pages but we had lot of trouble.
+For example, we lost one of our free server and also lost the player's scores.
+Currently, the game server is hosted by a guy with his ADSL connection. It's
+really unstable because the guy also does other things that generate some lag and
+also some downtime.</p>
+
+<p>To have a better game quality, we need our own servers and bandwidth and for
+that we need... money! We had to choose between several possibilities and we
+decided to keep the game completely free and ask you to help us.</p>
+
+<p>So, if you like this game and you want to support it, please donate! All the
+money will be used to host servers and increase the game quality. If we don't
+have enough money, this game will never be better and perhaps will close :(</p>
+
+<p>You are free to choose what you want to give, we setuped some limits that if
+you to go throw will enable you to be recognize as an official Mtp Target supporter.
+If you give 15 euros or more, your name will be displayed in the hall of supporters
+(if you want to be anonymous, just tell us). Of course we also accept if you give
+less than 15 euros.</p>
+
+<?php boxBegin(false); ?>
+15 Euros</td><td>Supporter</td></tr><tr><td>
+30 Euros</td><td>Contributors</td></tr><tr><td>
+60 Euros</td><td>Sponsors</td></tr><tr><td>
+More</td><td>some specific titles will be atributed
+<?php boxEnd(); ?>
+
+<p>Here it is the different system available to donate:</p>
+
+<h3>PayPal</h3>
+
+<p>You can donate on Secure Internet with this method. You only need a Visa or
+a Mastercard or an account on Paypal.</p>
+
+<p>Warning, with this method, a subpart of the value you give will be kept by Paypal
+(0.XXEuro + X.XX%).</p>
+
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+<input type="hidden" name="cmd" value="_xclick">
+<input type="hidden" name="business" value="acemtp@gmail.com">
+<input type="hidden" name="item_name" value="Dons Mtp Target">
+<input type="hidden" name="no_shipping" value="1">
+<input type="hidden" name="no_note" value="1">
+<input type="hidden" name="currency_code" value="EUR">
+<input type="hidden" name="tax" value="0">
+<input type="hidden" name="lc" value="US">
+Amount of money:<br>
+<input type="text" name="amount" size="2" value="15.00"> Euros<br/>
+<input type="image" src="https://www.paypal.com/en_US/i/btn/x-click-but04.gif" border="0" name="submit" alt="Donate with PayPal">
+</form>
+
+<!--
+<h3>Virement bancaire</h3>
+
+<p>Le virement bancaire vous permet d'être sûr que tout l'argent que vous
+donnez nous revient. Voici le compte à créditer:</p>
+
+<p>RIB (uniquement pour la France):
+<?php boxBegin(false); ?>
+Banque</td><td>Guichet</td><td>Numéro de compte</td><td>Clé</td>
+</tr><tr>
+<td>12548</td><td>02998</td><td>150175 115 03</td><td>61
+<?php boxEnd(); ?>
+</p>
+
+<p>IBAN (utilisation internationale):
+<?php boxBegin(false); ?>
+FR76 1254 8029 9815 0175 1150 361</td>
+</tr><tr>
+<td>Bank Indentification Code: AXABFRPP
+<?php boxEnd(); ?>
+</p>
+
+<h3>Chèque (uniquement pour la France)</h3>
+
+<p>Vous pouvez aussi tout simplement envoyer un chèque à l'ordre de Vianney
+Lecroart à l'adresse suivante:
+<?php boxBegin(false); ?>
+Lecroart Vianney</td></tr>
+<tr><td>Dons pour Mtp Target</td></tr>
+<tr><td>21 rue planchat</td></tr>
+<tr><td>75020 Paris</td></tr>
+<?php boxEnd(); ?>
+</p>
+-->
+
+<h3>Hall of supporters</h3>
+<table>
+<tr><td>Name</td><td>Dons</td></tr>
+
+<?php
+         break;
+     }
+?>
+
+<!--
+<tr><td></td><td>1 Euro</td></tr>
+-->
+</table>
