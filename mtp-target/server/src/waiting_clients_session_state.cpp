49ff88653ea792973a9d63589f15d036497bae96
Alban Lecocq <skeetmtp@gmail.com>
2005-06-01 19:32:49 +0000

    *** empty log message ***

--- mtp-target/server/src/waiting_clients_session_state.cpp
+++ mtp-target/server/src/waiting_clients_session_state.cpp
@@ -170,7 +170,7 @@ void CWaitingClientsSessionState::update()
 	{
 		if(_restart)
 		{
-			CNetwork::instance().sendToPublicChat("leave restart required");
+			CNetwork::instance().sendToPublicChat("quit restart required");
 			nlinfo("server restart required");
 			throw "server restart required";
 		}
